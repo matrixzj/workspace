62851ad9939ade411361986414bd397bd61b2780
Matrix Zou <matrix.zj@gmail.com>
2024-05-09 13:07:53 +0400

    There are 2 files have been modified at 2024-05-09 13:07:50 . 	-rwxrwxrwx 1 jun_zou jun_zou 2215 May  9 13:05 setup.sh 	-rwxrwxrwx 1 jun_zou jun_zou 4134 May  9 11:40 tmux/tmux.conf.rhel No files have been added. No files have been deleted. No file type has been changed.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-# tmux powerline theme setup
-if [ -d ~/downloads/tmux-powerline ]; then
-	rm -rf  ~/downloads/tmux-powerline
-fi
+## Tmux Plugin Manager Installation
+[ -d ~/.tmux ] || mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# tmux powerline theme setup
+[ -d ~/downloads/tmux-powerline ] && rm -rf  ~/downloads/tmux-powerline
 os=$(uname)
 
-cd  ~/downloads
-git clone https://github.com/erikw/tmux-powerline.git
+[ -d ~/downloads ] || mkdir -p ~/downloads
+git clone https://github.com/erikw/tmux-powerline.git ~/downloads/tmux-powerline
 
 sed -i.bak '/TMUX_POWERLINE_PATCHED_FONT_IN_USE_DEFAULT/s/true/false/' ~/downloads/tmux-powerline/config/defaults.sh
-sed -i.bak '/TMUX_POWERLINE_SEG_WEATHER_LOCATION/a\export TMUX_POWERLINE_SEG_WEATHER_LOCATION="2151330"' ~/downloads/tmux-powerline/segments/weather.sh
+# Location: Abu Dhabi 
+sed -i.bak '/TMUX_POWERLINE_SEG_WEATHER_LOCATION/a\TMUX_POWERLINE_SEG_WEATHER_LAT="24.47"' ~/downloads/tmux-powerline/segments/weather.sh
+sed -i.bak '/TMUX_POWERLINE_SEG_WEATHER_LOCATION/a\TMUX_POWERLINE_SEG_WEATHER_LON="54.37"' ~/downloads/tmux-powerline/segments/weather.sh
 sed -i.bak '/wan_ip/s/\(.*\)/#&1/;/pwd/s/\(.*\)/#&1/;/mailcount/s/\(.*\)/#&1/;/now_play/s/\(.*\)/#&1/;/battery/s/\(.*\)/#&1/;/load/s/\(.*\)/#&1/' ~/downloads/tmux-powerline/themes/default.sh
 
 if [[ ${os} = "Darwin" ]]; then
@@ -24,7 +27,7 @@ fi
 cat << EOF >> ~/.bashrc
 alias ta='tmux attach'
 alias ll='ls -al --color=auto'
-alias vim='/usr/local/bin/vim'
+# alias vim='/usr/local/bin/vim'
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 
@@ -32,7 +35,6 @@ alias pbpaste='xsel --clipboard --output'
 PS1='\n\
 \[\e[0;33m\]\u@\H\[\e[0;30;1m\]:\[\e[0;33m\]\w \[\e[0;31;1m\]· \@ \d ·\[\e[0m\] \n\
 \[\e[0;31m\]!\! \[\e[0;37;1m\]\$\[\e[0m\] '
-alias vim='/usr/local/bin/vim'
 # END ANSIBLE MANAGED BLOCK
 
 export HISTTIMEFORMAT="%d/%m/%y %T "
@@ -43,12 +45,14 @@ source ~/.bashrc
 
 # vim setup
 mkdir -p ~/.vim/colors
-ln -sf ~/Documents/workspace/vim/oblivion.vim ~/.vim/colors/oblivion.vim
-ln -sf ~/Documents/workspace/vim/oblivion.vim ~/.vim/colors/Oblivion.vim
-ln -sf ~/Documents/workspace/vim/vimrc ~/.vimrc
+ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/oblivion.vim
+ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/Oblivion.vim
+ln -sf $(pwd)/vim/vimrc ~/.vimrc
 
 # zsh setup
-cp ~/Documents/workspace/matrix.zsh-theme ~/.oh-my-zsh/themes/
+# Install: sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ -d ~/.oh-my-zsh/themes/ ] || mkdir -p ~/.oh-my-zsh/themes/
+cp $(pwd)/matrix.zsh-theme ~/.oh-my-zsh/themes/
 sed -i '/^ZSH_THEME/s/=.*/="matrix"/' ~/.zshrc
 echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
 echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
