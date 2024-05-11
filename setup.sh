6d03e8820a96b31bc380ac9e50f642c8b01f277d
Matrix Zou <matrix.zj@gmail.com>
2024-05-11 18:42:04 +0400

    There are        3 files have been modified at 2024-05-11 18:42:02 . 	-rwxr-xr-x@ 1 jzou  staff  1851 May 11 16:20 setup.sh 	-rw-r--r--@ 1 jzou  staff  5217 May 11 11:44 tmux/tmux.conf 	-rw-r--r--@ 1 jzou  staff  3443 May 11 18:41 tmux/tmux.conf.mac There are        1 files have been added at 2024-05-11 18:42:03. 	drwxr-xr-x@ 4 jzou  staff  128 May 11 16:21 tmux/tmux-powerline/ There are       64 files have been deleted at 2024-05-11 18:42:03. 	tmux-powerline 	tmux/powerline/AUTHORS 	tmux/powerline/LICENSE 	tmux/powerline/README.md 	tmux/powerline/color_palette.sh 	tmux/powerline/config/defaults.sh 	tmux/powerline/config/helpers.sh 	tmux/powerline/config/paths.sh 	tmux/powerline/config/shell.sh 	tmux/powerline/generate_rc.sh 	tmux/powerline/img/full.png 	tmux/powerline/img/left-status.png 	tmux/powerline/img/right-status.png 	tmux/powerline/img/right-status_no_mail.png 	tmux/powerline/img/right-status_no_mpd.png 	tmux/powerline/img/right-status_weather_battery.png 	tmux/powerline/lib/arg_processing.sh 	tmux/powerline/lib/formatting.sh 	tmux/powerline/lib/muting.sh 	tmux/powerline/lib/powerline.sh 	tmux/powerline/lib/rcfile.sh 	tmux/powerline/lib/text_roll.sh 	tmux/powerline/lib/tmux_adapter.sh 	tmux/powerline/mute_powerline.sh 	tmux/powerline/powerline.sh 	tmux/powerline/segments/Makefile 	tmux/powerline/segments/battery.sh 	tmux/powerline/segments/cpu.sh 	tmux/powerline/segments/date.sh 	tmux/powerline/segments/date_day.sh 	tmux/powerline/segments/earthquake.sh 	tmux/powerline/segments/hostname.sh 	tmux/powerline/segments/ifstat.sh 	tmux/powerline/segments/ifstat_sys.sh 	tmux/powerline/segments/lan_ip.sh 	tmux/powerline/segments/load.sh 	tmux/powerline/segments/macos_notification_count.sh 	tmux/powerline/segments/mailcount.sh 	tmux/powerline/segments/mailcount_apple_mail.script 	tmux/powerline/segments/mode_indicator.sh 	tmux/powerline/segments/now_playing.sh 	tmux/powerline/segments/np_itunes.script 	tmux/powerline/segments/np_mpd.c 	tmux/powerline/segments/np_pithos.py 	tmux/powerline/segments/np_rdio_mac.script 	tmux/powerline/segments/np_spotify_mac.script 	tmux/powerline/segments/pwd.sh 	tmux/powerline/segments/rainbarf.sh 	tmux/powerline/segments/time.sh 	tmux/powerline/segments/tmux_mem_cpu_load.sh 	tmux/powerline/segments/tmux_session_info.sh 	tmux/powerline/segments/uptime.sh 	tmux/powerline/segments/utc_time.sh 	tmux/powerline/segments/vcs_branch.sh 	tmux/powerline/segments/vcs_compare.sh 	tmux/powerline/segments/vcs_modified.sh 	tmux/powerline/segments/vcs_others.sh 	tmux/powerline/segments/vcs_revision.sh 	tmux/powerline/segments/vcs_staged.sh 	tmux/powerline/segments/wan_ip.sh 	tmux/powerline/segments/weather.sh 	tmux/powerline/segments/xkb_layout.c 	tmux/powerline/segments/xkb_layout.sh 	tmux/powerline/themes/default.sh No file type has been changed.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,52 @@
 #! /bin/bash
 
-## Tmux Plugin Manager Installation
-[ -d ~/.tmux ] || mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# tmux powerline theme setup
-[ -d ~/downloads/tmux-powerline ] && rm -rf  ~/downloads/tmux-powerline
 os=$(uname)
 
-[ -d ~/downloads ] || mkdir -p ~/downloads
-git clone https://github.com/erikw/tmux-powerline.git ~/downloads/tmux-powerline
+# Bash Config
+grep -q 'Matrix Customized BLOCK' ~/.bashrc
+if [ $? -eq 1 ]; then
+    cat << EOF >> ~/.bashrc
+# BEGIN Matrix Customized BLOCK
+alias ta='tmux attach'
+alias ll='ls -al --color=auto'
+# alias vim='/usr/local/bin/vim'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
 
-sed -i.bak '/TMUX_POWERLINE_PATCHED_FONT_IN_USE_DEFAULT/s/true/false/' ~/downloads/tmux-powerline/config/defaults.sh
-# Location: Abu Dhabi 
-sed -i.bak '/TMUX_POWERLINE_SEG_WEATHER_LOCATION/a\TMUX_POWERLINE_SEG_WEATHER_LAT="24.47"' ~/downloads/tmux-powerline/segments/weather.sh
-sed -i.bak '/TMUX_POWERLINE_SEG_WEATHER_LOCATION/a\TMUX_POWERLINE_SEG_WEATHER_LON="54.37"' ~/downloads/tmux-powerline/segments/weather.sh
-sed -i.bak '/wan_ip/s/\(.*\)/#&1/;/pwd/s/\(.*\)/#&1/;/mailcount/s/\(.*\)/#&1/;/now_play/s/\(.*\)/#&1/;/battery/s/\(.*\)/#&1/;/load/s/\(.*\)/#&1/' ~/downloads/tmux-powerline/themes/default.sh
+PS1='\n\
+\[\e[0;33m\]\u@\H\[\e[0;30;1m\]:\[\e[0;33m\]\w \[\e[0;31;1m\]· \@ \d ·\[\e[0m\] \n\
+\[\e[0;31m\]!\! \[\e[0;37;1m\]\$\[\e[0m\] '
+
+export HISTTIMEFORMAT="%d/%m/%y %T "
+export HISTFILESIZE=10000
+# END Matrix Customized BLOCK
+EOF
+fi
+
+source ~/.bashrc
+
+# oh-my-zsh Config
+## Install: sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ -d ~/.oh-my-zsh/themes/ ] || mkdir -p ~/.oh-my-zsh/themes/
+
+cp $(pwd)/matrix.zsh-theme ~/.oh-my-zsh/themes/
+sed -i '/^ZSH_THEME/s/=.*/="matrix"/' ~/.zshrc
+
+grep -q 'Matrix Customized BLOCK' ~/.zshrc
+if [ $? -eq 1 ]; then
+    cat << EOF >> ~/.zshrc
+# BEGIN Matrix Customized BLOCK
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+# END Matrix Customized BLOCK
+EOF
+fi
+
+source ~/.zshrc
+
+# Tmux Config
+## Tmux Plugin Manager Installation
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 if [[ ${os} = "Darwin" ]]; then
     ln -sf ~/Documents/workspace/tmux/tmux.conf.mac ~/.tmux.conf
@@ -23,36 +54,12 @@ else
     ln -sf ~/Documents/workspace/tmux/tmux.conf ~/.tmux.conf
 fi
 
-# bashrc setup
-cat << EOF >> ~/.bashrc
-alias ta='tmux attach'
-alias ll='ls -al --color=auto'
-# alias vim='/usr/local/bin/vim'
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
-
-# BEGIN ANSIBLE MANAGED BLOCK
-PS1='\n\
-\[\e[0;33m\]\u@\H\[\e[0;30;1m\]:\[\e[0;33m\]\w \[\e[0;31;1m\]· \@ \d ·\[\e[0m\] \n\
-\[\e[0;31m\]!\! \[\e[0;37;1m\]\$\[\e[0m\] '
-# END ANSIBLE MANAGED BLOCK
-
-export HISTTIMEFORMAT="%d/%m/%y %T "
-export HISTFILESIZE=10000
-EOF
-
-source ~/.bashrc
-
-# vim setup
-mkdir -p ~/.vim/colors
-ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/oblivion.vim
-ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/Oblivion.vim
-ln -sf $(pwd)/vim/vimrc ~/.vimrc
-
-# zsh setup
-# Install: sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-[ -d ~/.oh-my-zsh/themes/ ] || mkdir -p ~/.oh-my-zsh/themes/
-cp $(pwd)/matrix.zsh-theme ~/.oh-my-zsh/themes/
-sed -i '/^ZSH_THEME/s/=.*/="matrix"/' ~/.zshrc
-echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
-echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
+[ -d ~/.tmux/plugins/tmux-powerline ] || mkdir -p ~/.tmux/plugins/tmux-powerline/themes
+ln -sf $(pwd)/tmux/tmux-powerline/config.sh ~/.config/tmux-powerline/config.sh
+ln -sf $(pwd)/tmux/tmux-powerline/themes/matrix.sh ~/.config/tmux-powerline/themes/matrix.sh
+ 
+# # VIM Config
+# mkdir -p ~/.vim/colors
+# ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/oblivion.vim
+# ln -sf $(pwd)/vim/oblivion.vim ~/.vim/colors/Oblivion.vim
+# ln -sf $(pwd)/vim/vimrc ~/.vimrc
